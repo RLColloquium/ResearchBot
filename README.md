fcae288fbab5c68eabfb9245dc362a06c58d4646
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2020-07-26 16:49:24 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,248 @@
 # ResearchBot
-Slack で arXiv 等の URL を書くとタイトルやアブストラクト等を通知するボット
+
+[Slack](https://slack.com/) で [arXiv](https://arxiv.org/) 等の URL を書くとタイトルやアブストラクト等を通知するボット.
+
+[DeepL API](https://www.deepl.com/docs-api/introduction/) での翻訳にも対応しています.
+
+ここでは, まずローカル環境でテストして, その後, 本番環境として [Heroku](https://www.heroku.com/) で ResearchBot をホスティングする方法を説明します.
+
+
+## Slack App の設定(前半)
+
+以下のページを参考に Slack App を作成して Bot User OAuth Access Token を生成するところまで設定します.
+
+https://qiita.com/seratch/items/a001985ee1dccaf95727#slack-%E3%82%A2%E3%83%97%E3%83%AA%E3%82%92%E4%BD%9C%E6%88%90
+
+まず, 以下のページから Slack App を作成します.
+
+https://api.slack.com/apps?new_app=1
+
+以下のように入力して Create App します.
+
+App Name: ResearchBot
+Development Slack Workspace: (ワークスペース)
+
+
+左のメニューから,
+
+Features > OAuth & Permissions
+
+Scopes > Bot Token Scopes
+
+の部分に
+
+```
+channels:history
+groups:history
+chat:write
+```
+
+を選択します.
+
+左のメニューから,
+
+Features > App Home
+App Display Name > Edit
+
+Display Name (Bot Name): ResearchBot
+Default username: researchbot
+
+して Save を押します. その下の以下項目を ON にしておきます.
+
+
+Always Show My Bot as Online: ON
+
+Show Tabs > Home Tab: ON
+
+
+左のメニューから
+
+Settings > Install App
+
+Install App to Workspace
+
+Allow を押します.
+
+Bot User OAuth Access Token (`xoxb-nnn...`) をメモしておきます.
+
+
+同様に 左のメニューから
+
+Settings > Basic Information > App Credentials > Signing Secret > Show
+
+Signing Secret もメモしておきます.
+
+
+Slackの設定は一旦ここまでやっておいて, 後で残りの設定をします.
+
+
+## Slack App の招待
+
+作った Slack App をテスト用の Slack チャンネルに招待しておきます.
+
+例: ボットの名前を ResearchBot にした場合, 招待したいチャンネルで,
+
+```
+/invite @ResearchBot
+```
+
+
+## ローカルの環境の設定
+
+上記でメモした Slack App の API Token を環境変数 `SLACKBOT_API_TOKEN` に, Signing Secret を `SLACK_SIGNING_SECRET` に設定しておきます.
+
+ローカルでテストする場合は, 環境変数を ~/.zshrc に書いておきます.
+
+```sh
+export SLACK_BOT_TOKEN="xoxb-nnn..."
+```
+
+```sh
+export SLACK_SIGNING_SECRET="xxx..."
+```
+
+DeepL API を使う場合は, 同様に `DEEPL_AUTH_KEY` も設定しておきます.
+
+
+ローカル環境の設定と動作確認を以下のようにします. ngrok が必要です.
+
+```sh
+# ngrok をインストールしていない場合は以下でインストール(Macの場合)
+# brew cask install ngrok
+git clone git@github.com:RLColloquium/ResearchBot.git
+cd ResearchBot
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python run.py
+# 別ターミナルで
+ngrok http 3000
+```
+
+ngrok のアドレスをメモしておきます.
+
+## Slack App の設定(後半)
+
+Slack App の画面に戻って,
+
+Features > Event Subscriptions
+
+Enable Events > Request URL
+
+先程メモしておいた ngrok のアドレスの末尾に `/slack/events` を追加して入力します.
+
+```
+https://xxx.ngrok.io/slack/events
+```
+
+Verified と表示されれば OK です.
+
+
+Enable Events > Subscribe to bot events
+
+```
+message.channels
+message.groups
+```
+
+を追加して `Save Changes` を押します.
+
+Slack の ResearchBot を招待したチャンネルで `https://arxiv.org/abs/2005.05960` と発言してみてボットの返答がスレッドでつけばローカル環境でのテストは完了です. ngrok と run.py を Ctrl-C で終了しておきます.
+
+
+## Heroku での設定
+
+ローカル環境でのテストが十分できたら Heroku にデプロイする設定をします.
+
+
+### Heroku アカウントの作成と CLI のインストール
+
+以下のチュートリアルを参考に, Heroku アカウントの作成から CLI のインストールまで済ませておきます(Postgres をインストールする必要はありません).
+
+https://devcenter.heroku.com/articles/getting-started-with-python
+
+
+### Heroku アプリ作成
+
+Heroku のアプリ作成を以下のようにします.
+
+```sh
+heroku login
+heroku create rl-colloquium-researchbot  # 任意のアプリ名を指定.
+git remote -v  # heroku が追加されているか確認
+heroku buildpacks:set heroku/python
+# Selenium と Chrome を使う場合は以下
+# heroku buildpacks:set https://github.com/heroku/heroku-buildpack-chromedriver.git
+# heroku buildpacks:set https://github.com/heroku/heroku-buildpack-google-chrome.git
+```
+
+### Heroku へのデプロイ
+
+Heroku へのデプロイは以下のようにします.
+
+```sh
+git push heroku master  # Heroku にコードをアップロード
+```
+
+### Heroku 環境変数設定
+
+以前メモした Slack App の 設定を を環境変数 `SLACKBOT_API_TOKEN`, `SLACK_SIGNING_SECRET` で設定しておきます. DeepL API を使う場合は, 同様に `DEEPL_AUTH_KEY` も設定しておきます.
+
+以下ページを参考に, Heroku 側での環境変数を設定します.
+
+https://devcenter.heroku.com/articles/config-vars
+
+CLI のコマンドか Web で設定できますが, CLI だとシェルのヒストリに残ってしまうので Web で設定した方が良いかもしれません.
+
+https://devcenter.heroku.com/articles/config-vars#using-the-heroku-dashboard
+
+
+### Heroku アプリ起動
+
+以下ページを参考に, Heroku アプリを起動します.
+
+https://qiita.com/akabei/items/ec5179794f9e4e1df203#%E8%B5%B7%E5%8B%95
+
+
+### Heroku ログ確認
+
+別ターミナルを開いて, ログを確認しておきます.
+
+```
+heroku logs --tail
+```
+
+
+## 開発サイクル
+
+以下のようなサイクルで開発しています.
+
+```sh
+# ローカルでテスト
+# コードを変更
+python run.py
+ngrok http 3000
+# Slack App の設定で Features > Event Subscriptions, Enable Events > Request URL を ngrok の URL に変更
+# Slack で動作確認
+# Heroku にアップロード
+git add
+git commit
+git push heroku master # Heroku にアップロードされてコードが更新
+# Slack App の設定で Features > Event Subscriptions, Enable Events > Request URL を Heroku の URL に変更
+# Slack で動作確認
+```
+
+## 参考
+
+- https://devcenter.heroku.com/articles/getting-started-with-python
+- https://api.slack.com/bot-users
+- https://www.deepl.com/docs-api/introduction/
+- https://qiita.com/seratch/items/a001985ee1dccaf95727
+- https://qiita.com/akabei/items/ec5179794f9e4e1df203
+- https://qiita.com/nsuhara/items/76ae132734b7e2b352dd
+
+
+## 作者
+
+Susumu OTA
+
---
+++ run.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2020 Susumu OTA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import re
+import time
+import urllib
+from functools import lru_cache
+# from pprint import PrettyPrinter
+import requests
+from flask import Flask, request
+from slackeventsapi import SlackEventAdapter
+from slack import WebClient
+from arxiv import query
+
+# import translator # for another translation api
+
+
+TRANSLATE_TEXT_CACHE_MAXSIZE = 512 # TODO: optimize memory vs hits rate
+
+# pp = PrettyPrinter()
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello!'
+
+
+slack_events_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_SECRET'], '/slack/events', app)
+slack_client = WebClient(os.environ['SLACK_BOT_TOKEN'])
+
+
+def is_valid_slack_user_id(user_id):
+    # https://github.com/slackapi/slack-api-specs/blob/master/web-api/slack_web_openapi_v2.json
+    # "defs_user_id": { "pattern": "^[UW][A-Z0-9]{2,}$", ... }
+    return re.match(r'^[UW][A-Z0-9]{2,10}$', user_id) # TODO: upper limit seems like 10 but not sure
+
+def get_deepl_auth_key(user_id):
+    if is_valid_slack_user_id(user_id) and os.getenv('DEEPL_AUTH_KEY_{}'.format(user_id)):
+        # print('Found a user specific deepl auth key: {}'.format(user_id))
+        return os.getenv('DEEPL_AUTH_KEY_{}'.format(user_id)) # user specific auth key
+    else:
+        return os.getenv('DEEPL_AUTH_KEY') # default auth key
+        # return None # or just reject
+
+@lru_cache(maxsize=TRANSLATE_TEXT_CACHE_MAXSIZE)
+def translate_text(text, target_lang='JA'): # drop user_id to increase cache hits rate
+    global user_id # set user_id global for effective lru_cache. see handle_message.
+    deepl_auth_key = get_deepl_auth_key(user_id)
+    if deepl_auth_key:
+        return translate_deepl_api(text, deepl_auth_key, target_lang=target_lang)
+    # else:
+    #     return translator.translate_another_api(text, target_lang=target_lang)
+    else:
+        return None
+
+def translate_deepl_api(text, auth_key, target_lang='JA'):
+    # https://www.deepl.com/docs-api/translating-text/
+    start = time.time()
+    params = {
+        'auth_key': auth_key,
+        'text': text,
+        'target_lang': target_lang
+    }
+    r = requests.post('https://api.deepl.com/v2/translate', data=params)
+    if r.status_code == requests.codes.ok:
+        j = r.json()
+        # pp.pprint(j)
+        print('translate_deepl_api: {:.5f} sec'.format(time.time() - start))
+        return j['translations'][0]['text'] if 'translations' in j else None # TODO: need more check?
+    else:
+        print('Failed to translate: {}'.format(r.text))
+        return None
+
+def generate_response(r):
+    arxiv_id = re.sub(r'https?://arxiv.org/abs/([0-9v\.]+)', r'\1', r['id'])
+    arxiv_id_no_v = re.sub(r'v[0-9]+$', r'', arxiv_id)
+    vanity_url = 'https://www.arxiv-vanity.com/papers/{}/'.format(arxiv_id_no_v)
+    tweets_url = 'https://twitter.com/search?q=arxiv.org%2Fabs%2F{}&f=live'.format(arxiv_id_no_v)
+    tags = ' | '.join(['{}'.format(t['term']) for t in r['tags']])
+    u = r['updated_parsed']
+    p = r['published_parsed']
+    date = '{:04d}/{:02d}/{:02d}, {:04d}/{:02d}/{:02d}'.format(p.tm_year, p.tm_mon, p.tm_mday, u.tm_year, u.tm_mon, u.tm_mday)
+    comment = r['arxiv_comment'] or ''
+    vanity = '<{}|vanity>'.format(vanity_url)
+    tweets = '<{}|{}>'.format(tweets_url, 'tweets')
+    summary = re.sub(r'\n', r' ', r['summary'])
+    translation = translate_text(summary)
+    summary = translation if translation and len(translation) > 0 else summary
+    # summary = '\n'.join([translation, summary])
+    lines = [
+        # r['id'],
+        re.sub(r'\n', r' ', r['title']),
+        ', '.join(r['authors']),
+        ', '.join([date, vanity, tweets, tags, comment]),
+        summary
+    ]
+    return '\n'.join(lines)
+
+def is_retry_request(request):
+    return request.headers.get('X-Slack-Retry-Num') and request.headers.get('X-Slack-Retry-Reason') == 'http_timeout'
+
+def is_user(e):
+    return e.get('user') and e.get('bot_id') is None
+
+def get_arxiv_id(text):
+    m = re.search(r'https?://arxiv\.org/(abs|pdf)/([0-9v\.]+)(\.pdf)?', text)
+    if m and m.group(2):
+        return m.group(2)
+    #m = re.search(r'https?://arxiv\.org/pdf/([0-9v\.]+).pdf', text)
+    #if m and m.group(1):
+    #    return m.group(1)
+    return None
+
+
+@slack_events_adapter.on('message')
+def handle_message(event):
+    if is_retry_request(request): # flask.request
+        # TODO: use asyncio or multithread or whatever to avoid retry events. it needs to respond "200 OK" within 3 seconds.
+        # print('Slack Events API retry event: ignored')
+        return
+    e = event['event']
+    if not is_user(e): # bot
+        # print('Bot message: ignored')
+        return
+    arxiv_id = get_arxiv_id(e['text'])
+    if arxiv_id is None: # not arXiv url
+        # print('Not arXiv URL: ignored')
+        return
+    rs = query(id_list=[arxiv_id])
+    if rs and len(rs) > 0:
+        global user_id # set user_id global for effective lru_cache. see translate_text.
+        user_id = e['user']
+        text = generate_response(rs[0]) # generate_response(rs[0], user_id)
+    else:
+        text = 'No result found: {}'.format(arxiv_id)
+    slack_client.chat_postMessage(channel=e['channel'], text=text, thread_ts=e['ts'])
+
+@slack_events_adapter.on('error')
+def handle_error(err):
+    print('Error: {}'.format(str(err)))
+
+
+if __name__ == '__main__':
+    port = os.getenv('PORT') or 3000
+    app.run(port=port)
