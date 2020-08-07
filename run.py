60c988c87131f611fa3f3473bb73a799861958cd
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2020-08-08 00:54:34 +0900

    Add toptweets command

--- run.py
+++ run.py
@@ -17,20 +17,23 @@
 import os
 import re
 import time
+import json
 import urllib
+from operator import attrgetter
 from functools import lru_cache
 from concurrent.futures import ThreadPoolExecutor
 # from pprint import PrettyPrinter
 import requests
+import pandas as pd
+import numpy as np
 from flask import Flask, request
 from slackeventsapi import SlackEventAdapter
 from slack import WebClient
-from arxiv import query
+import arxiv
+import tweepy
 
 # import translator # for another translation api
 
-
-TRANSLATE_TEXT_CACHE_MAXSIZE = 512 # TODO: optimize memory vs hits rate
 
 # pp = PrettyPrinter()
 
@@ -44,6 +47,16 @@ def hello():
 slack_events_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_SECRET'], '/slack/events', app)
 slack_client = WebClient(os.environ['SLACK_BOT_TOKEN'])
 
+def get_twitter_api():
+    if os.getenv('TWITTER_API_KEY') and os.getenv('TWITTER_API_SECRET_KEY') and os.getenv('TWITTER_ACCESS_TOKEN') and os.getenv('TWITTER_ACCESS_TOKEN_SECRET'):
+        #auth = tweepy.OAuthHandler(os.environ['TWITTER_API_KEY'], os.environ['TWITTER_API_SECRET_KEY'])
+        #auth.set_access_token(os.environ['TWITTER_ACCESS_TOKEN'], os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
+        auth = tweepy.AppAuthHandler(os.environ['TWITTER_API_KEY'], os.environ['TWITTER_API_SECRET_KEY'])
+        return tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    else:
+        return None
+
+twitter_api = get_twitter_api()
 
 def is_valid_slack_user_id(user_id):
     # https://github.com/slackapi/slack-api-specs/blob/master/web-api/slack_web_openapi_v2.json
@@ -58,7 +71,7 @@ def get_deepl_auth_key(user_id):
         return os.getenv('DEEPL_AUTH_KEY') # default auth key
         # return None # or just reject
 
-@lru_cache(maxsize=TRANSLATE_TEXT_CACHE_MAXSIZE)
+@lru_cache(maxsize=128)
 def translate_text(text, target_lang='JA'): # drop user_id to increase cache hits rate
     global user_id # set user_id global for effective lru_cache. see handle_message.
     deepl_auth_key = get_deepl_auth_key(user_id)
@@ -87,23 +100,23 @@ def translate_deepl_api(text, auth_key, target_lang='JA'):
         return None
 
 def generate_response(r):
-    arxiv_id = re.sub(r'https?://arxiv.org/abs/([0-9v\.]+)', r'\1', r['id'])
-    arxiv_id_no_v = re.sub(r'v[0-9]+$', r'', arxiv_id)
+    arxiv_id = get_arxiv_id(r['id'])
+    arxiv_id_no_v = get_arxiv_id_no_v(arxiv_id)
     vanity_url = 'https://www.arxiv-vanity.com/papers/{}/'.format(arxiv_id_no_v)
-    tweets_url = 'https://twitter.com/search?q=arxiv.org%2Fabs%2F{}&f=live'.format(arxiv_id_no_v)
+    tweets_url = 'https://twitter.com/search?q=arxiv.org%2Fabs%2F{}%20OR%20arxiv.org%2Fpdf%2F{}.pdf%20&f=live'.format(arxiv_id_no_v, arxiv_id_no_v)
     tags = ' | '.join(['{}'.format(t['term']) for t in r['tags']])
     u = r['updated_parsed']
     p = r['published_parsed']
     date = '{:04d}/{:02d}/{:02d}, {:04d}/{:02d}/{:02d}'.format(p.tm_year, p.tm_mon, p.tm_mday, u.tm_year, u.tm_mon, u.tm_mday)
     comment = r['arxiv_comment'] or ''
     vanity = '<{}|vanity>'.format(vanity_url)
-    tweets = '<{}|{}>'.format(tweets_url, 'tweets')
+    tweets = '<{}|{} tweets>'.format(tweets_url, r['num_tweets'] if 'num_tweets' in r else '?')
     summary = re.sub(r'\n', r' ', r['summary'])
     translation = translate_text(summary)
     summary = translation if translation and len(translation) > 0 else summary
     # summary = '\n'.join([translation, summary])
     lines = [
-        # r['id'],
+        r['id'],
         re.sub(r'\n', r' ', r['title']),
         ', '.join(r['authors']),
         ', '.join([date, vanity, tweets, tags, comment]),
@@ -111,7 +124,7 @@ def generate_response(r):
     ]
     return '\n'.join(lines)
 
-def is_retry_request(request):
+def is_retry_request(request): # flask.request
     return request.headers.get('X-Slack-Retry-Num') and request.headers.get('X-Slack-Retry-Reason') == 'http_timeout'
 
 def is_user(e):
@@ -121,39 +134,131 @@ def get_arxiv_id(text):
     m = re.search(r'https?://arxiv\.org/(abs|pdf)/([0-9]+\.[0-9v]+)(\.pdf)?', text)
     return m.group(2) if m and m.group(2) else None
 
-def handle_arxiv_url(event):
-    e = event['event']
-    arxiv_id = get_arxiv_id(e['text'])
-    if arxiv_id is None: # not arXiv url
-        # print('Not arXiv URL: ignored')
-        return
-    rs = query(id_list=[arxiv_id])
+def find_all_unique_arxiv_ids(text):
+    m = re.findall(r'https?://arxiv\.org/(abs|pdf)/([0-9]+\.[0-9v]+)(\.pdf)?', text)
+    return list(set([get_arxiv_id_no_v(item[1]) for item in m])) if m else []
+
+def get_arxiv_id_no_v(arxiv_id):
+    return re.sub(r'v[0-9]+$', r'', arxiv_id)
+
+def handle_arxiv_url(e):
+    arxiv_id = get_arxiv_id(e['text']) # arxiv_id was already checked so it should not be None
+    # rs = arxiv.query(id_list=[arxiv_id])
+    rs = arxiv_query(id_list_str=list_to_str([arxiv_id]))
     if rs and len(rs) > 0:
-        global user_id # set user_id global for effective lru_cache. see translate_text.
-        user_id = e['user']
-        text = generate_response(rs[0]) # generate_response(rs[0], user_id)
+        r = rs[0]
+        arxiv_id_no_v = get_arxiv_id_no_v(arxiv_id)
+        arxiv_id_counts = get_tweeted_arxiv_id_counts('"arxiv.org/abs/{}" OR "arxiv.org/pdf/{}.pdf"'.format(arxiv_id_no_v, arxiv_id_no_v))
+        r['arxiv_id_no_v'] = arxiv_id_no_v
+        r['num_tweets'] = 0 if len(arxiv_id_counts) < 1 else arxiv_id_counts[0]
+        text = generate_response(r) # generate_response(r, user_id)
     else:
         text = 'No result found: {}'.format(arxiv_id)
     slack_client.chat_postMessage(channel=e['channel'], text=text, thread_ts=e['ts'])
 
 
+def get_toptweets_args(text, default_max_results=5):
+    m = re.match(r'^toptweets(\s+([0-9]+))?$', text)
+    return (int(m.group(2)) if m.group(2) else default_max_results) if m else None
+
+@lru_cache(maxsize=128)
+def get_tweeted_arxiv_id_counts(q):
+    start = time.time()
+    i = 0
+    arxiv_ids = []
+    try:
+        # https://developer.twitter.com/en/docs/basics/rate-limits
+        for status in tweepy.Cursor(twitter_api.search, q=q, count=100, result_type='recent', tweet_mode='extended').items(100*100):
+            ids = find_all_unique_arxiv_ids(str(status._json)) # TODO: _json is a private member
+            arxiv_ids.extend(ids)
+            print(i) if i % 100 == 0 else None
+            i += 1
+    except Exception as e:
+        print('Exception: {}'.format(str(e)))
+    df = pd.DataFrame(arxiv_ids, columns=['arxiv_id'])
+    counts = df['arxiv_id'].value_counts()
+    print(len(df), len(counts))
+    print('get_tweeted_arxiv_id_counts: {:.6f} sec'.format(time.time() - start))
+    return counts
+
+def list_to_str(lst): # just a tiny hack to enable lru_cache for arxiv_query
+    return json.dumps(lst)
+
+def str_to_list(s): # just a tiny hack to enable lru_cache for arxiv_query
+    return json.loads(s)
+
+@lru_cache(maxsize=128)
+def arxiv_query(id_list_str='', q='', max_chunk_id_list=250): # list is unhashable, so it needs to convert from list to string to enable lru_cache
+    start = time.time()
+    id_list = str_to_list(id_list_str)
+    rs = []
+    cdr = id_list
+    try:
+        for i in range(1+len(id_list)//max_chunk_id_list): # avoid "HTTP Error 414 in query" (URI Too Long)
+            car = cdr[:max_chunk_id_list]
+            cdr = cdr[max_chunk_id_list:]
+            print(len(car), len(''.join(car)))
+            r = arxiv.query(id_list=car, query=q) # this will automatically sleep
+            rs.extend(r)
+    except Exception as e:
+        print('Exception: {}'.format(str(e)))
+    print('arxiv_query: {:.6f} sec'.format(time.time() - start))
+    return rs
+
+def handle_popular_arxiv(e):
+    max_results = get_toptweets_args(e['text'], default_max_results=5)
+    max_results = 10 if max_results > 10 else max_results
+    max_results = 1 if max_results < 1 else max_results
+    arxiv_id_counts = get_tweeted_arxiv_id_counts('"arxiv.org"')
+    if arxiv_id_counts is None or len(arxiv_id_counts) < 1:
+        text = 'No twitter result found'
+        slack_client.chat_postMessage(channel=e['channel'], text=text, thread_ts=e['ts'])
+        return
+    id_list = arxiv_id_counts.keys().tolist()
+    q = 'cat:cs.CV OR cat:cs.AI OR cat:cs.LG OR cat:cs.CL OR cat:cs.NE OR cat:stat.ML'
+    rs = arxiv_query(id_list_str=list_to_str(id_list), q=q, max_chunk_id_list=250)
+    print(len(id_list), len(rs))
+    if len(rs) < 1:
+        text = 'No arXiv result found'
+        slack_client.chat_postMessage(channel=e['channel'], text=text, thread_ts=e['ts'])
+        return
+    i = 0
+    for r in rs:
+        arxiv_id = get_arxiv_id(r['id'])
+        arxiv_id_no_v = get_arxiv_id_no_v(arxiv_id)
+        r['arxiv_id_no_v'] = arxiv_id_no_v
+        r['num_tweets'] = arxiv_id_counts[arxiv_id_no_v] if arxiv_id_no_v in arxiv_id_counts else 0
+        print(i, r['arxiv_id_no_v'], r['num_tweets']) if r['num_tweets'] == 0 else None
+        i += 1
+    rs.sort(key=attrgetter('num_tweets'), reverse=True)
+    rs = rs[:max_results]
+    for r in rs:
+        text = generate_response(r)
+        slack_client.chat_postMessage(channel=e['channel'], text=text, thread_ts=e['ts'])
+
+
 @slack_events_adapter.on('message')
 def handle_message(event):
-    start = time.time()
     if is_retry_request(request): # flask.request
-        # it needs to respond "200 OK" within 3 seconds.
-        print('retry event request')
+        # Slack Events API needs to respond "200 OK" within 3 seconds.
+        # if it came here, previous request should not be handled properly.
+        print('Retry event request')
         # return
     e = event['event']
-    if not is_user(e): # bot
+    if not is_user(e):
         # print('Bot message: ignored')
         return
+    global user_id # set user_id global for effective lru_cache. see translate_text.
+    user_id = e['user']
     if get_arxiv_id(e['text']):
         executor = ThreadPoolExecutor()
-        future = executor.submit(handle_arxiv_url, event) # non blocking
-        # print(future)
+        future = executor.submit(handle_arxiv_url, e) # non blocking
         executor.shutdown(wait=False) # non blocking
-        print('handle_message: {:.6f} sec'.format(time.time() - start))
+        return
+    if get_toptweets_args(e['text']):
+        executor = ThreadPoolExecutor()
+        future = executor.submit(handle_popular_arxiv, e) # non blocking
+        executor.shutdown(wait=False) # non blocking
         return
     # print('No keyword: ignored')
     return
